9cfde90af32045c981aeaacf9cb0eff0474f5c44
Josh <82674021+josh894@users.noreply.github.com>
2024-04-17 12:59:39 -0400

    Add External link buttons

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -8,6 +8,14 @@ function App() {
 
             <div className="full-width">
                 <h2 className = 'title-text'>Joshua Davidson</h2>
+                <div className = 'link' >
+                  <a href="https://github.com/josh894" target="_blank" rel="noopener noreferrer">
+                    <img className = 'photo-link' alt='thumbnail' src='./Git.png' onMouseOver={e => (e.currentTarget.src = "./GitHover.png")} onMouseOut={e => (e.currentTarget.src = "./Git.png")}></img>
+                  </a>
+                  <a href="https://www.linkedin.com/in/joshua-davidson-55236820b/" target="_blank" rel="noopener noreferrer">
+                    <img className = 'photo-link' alt='thumbnail' src='./LinkedIn.webp' onMouseOver={e => (e.currentTarget.src = "./LinkedinHover.webp")} onMouseOut={e => (e.currentTarget.src = "./LinkedIn.webp")}></img>
+                  </a>
+                </div>
             </div>
           
       <section className = 'body-text'>
@@ -19,7 +27,7 @@ function App() {
                                         to learn from others and am always searching for a bigger challenge to face. <br></br><br></br> 
 
                                         My current proficiencies are within the world of Data Science using Python and C, but I am willing and eager to learn
-                                        other sectors and languages within the software developing industry. Check out my resume for more info! <br></br><br></br> 
+                                        about other sectors and languages within the software developing industry. Check out my <b>resume</b> for more info! <br></br><br></br> 
 
                                         When I'm not working on personal projects, you can find me coordinating with my guild in intense and strategic mythic raiding in World of Warcraft.
                                         </p>
@@ -36,7 +44,7 @@ function App() {
             <img className = 'photo-thumb' alt='thumbnail' src='./GBADsLogoRedesign.png'></img>
             <div className = 'item-text'>
               <p className="career-title">Software Developer @ GBADs (2022-2024)</p>
-              <p className="career-description">• Created a Full-Stack <a href='https://gbadske.org/dashboards/visualizer/'>Plotly dashboard</a> to provide additional context for large population datasets.</p> 
+              <p className="career-description">• Created a Full-Stack <a href='https://gbadske.org/dashboards/visualizer/' target="_blank" rel="noopener noreferrer">Plotly dashboard</a> to provide additional context for large population datasets.</p> 
               <p className="career-description"> • Expanded Python literacy and knowledge on algorithms/dataframes in order to properly display users large datasets in a timely manner.</p> 
               <p className="career-description"> • Work directly contributed to reporting faulty data to the United Nations to be changed for future researchers
                                                 to work with accurate population data.</p> 
@@ -56,7 +64,7 @@ function App() {
             <img className = 'photo-thumb' alt='thumbnail' src='./SimpleArmory.jpg'></img>
             <div className = 'item-text'>
               <p className="item-title">Simple Armory</p>
-              <p className="item-description">• Consistent contributor to Kevin Clement's open source <a href='https://simplearmory.com/#/us/kelthuzad/asmongold/collectable/mounts'>website</a>.</p> 
+              <p className="item-description">• Consistent contributor to Kevin Clement's <a href='https://simplearmory.com/#/us/kelthuzad/asmongold/collectable/mounts' target="_blank" rel="noopener noreferrer">open source website</a>.</p> 
               <p className="item-description">• Up-kept json files that held information on hundreds of items updated monthly and refactored archaic json structures.</p> 
             </div>
         </div>
